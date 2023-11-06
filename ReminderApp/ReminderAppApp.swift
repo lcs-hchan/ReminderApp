8ff5f6d055df0789ed23bfc1fe00f15db24885b0
Hayden Chan <hchan@lcs.on.ca>
2023-11-06 14:35:40 -0500

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Hayden Chan on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
